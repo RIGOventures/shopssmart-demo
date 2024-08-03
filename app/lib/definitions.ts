40e501fba8ca21101ac365805b0796c5fde3ad33
Oluwanifemi Fadare <79525647+offad@users.noreply.github.com>
2024-08-03 22:56:39 +0100

    Add request data definition

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,6 +1,12 @@
 // This file contains type definitions for data.
 // It describes the shape of the data, and what data type each property should accept.
 
+
+interface UserRequestData {
+	count: number;
+	lastResetTime: number;
+}
+
 export type User = {
     id: string;
     name: string;
---
+++ app/lib/seed-data.ts
@@ -0,0 +1,17 @@
+// This file contains seed data for starting up the database
+const users = [
+	{
+		id: '0',
+		name: 'Admin',
+		email: 'admin@smart-shop.com',
+		password: 'admin',
+	},
+	{
+		id: '410544b2-4001-4271-9855-fec4b6a6442a',
+		name: 'User',
+		email: 'user@example.com',
+		password: '123456',
+	},
+];
+
+export { users };
